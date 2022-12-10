fee93b7b0ba8b6256e9705716e470dac51156dfb
Francisco <jgosmus@gmail.com>
2022-12-10 03:27:35 +0100

    Frontend loads transactions

--- backend/src/main/java/com/facespedes/finanzas/security/SecurityConfig.java
+++ backend/src/main/java/com/facespedes/finanzas/security/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -15,9 +20,21 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors().and()
+                .csrf().disable()
                 .authorizeHttpRequests().anyRequest().permitAll();
 
         return http.build();
     }
 
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
---
+++ frontend/pages/transactionsTable.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import {Table} from "antd";
+
+const columns = [
+    {title: 'Transactions', dataIndex: 'transactions', width: 100, fixed: 'left', children: [
+        {title: 'Date', dataIndex: 'date', width: '8%'},
+        {title: 'Concept', dataIndex: 'concept', width: '50%'},
+        {title: 'Amount', dataIndex: 'amount', width: '8%'},
+        {title: 'Category', dataIndex: 'category', width: '8%'},
+        {title: 'Note', dataIndex: 'note'}
+        ]
+    }
+    ];
+
+
+const TransactionsTable = () => {
+    const [loading, setLoading] = useState(true);
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/').then(response => response.json()).then(data => {
+            setLoading(false);
+            console.log(data);
+            const transactions = data.map((transaction) => {
+                return {
+                    key: transaction.id,
+                    date: transaction.valueDate,
+                    concept: transaction.concept,
+                    amount: transaction.transactionAmount.amount,
+                    category: "Unasigned",
+                    note: ""
+                }
+            });
+
+            setTransactions(transactions);
+        });
+    }, []);
+    return <Table columns={columns} dataSource={transactions} loading={loading}/>;
+};
+
+export default TransactionsTable;
